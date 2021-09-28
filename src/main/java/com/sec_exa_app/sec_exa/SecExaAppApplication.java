0e1eadaade07d0a9380e93177890d0a25e427d46
oag-dev <otajonazamov2001@gmail.com>
2021-09-28 13:54:06 +0500

    first commit

---
+++ src/main/java/com/sec_exa_app/sec_exa/SecExaAppApplication.java
@@ -0,0 +1,13 @@
+package com.sec_exa_app.sec_exa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecExaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecExaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sec_exa_app/sec_exa/SecExaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sec_exa_app.sec_exa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecExaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
